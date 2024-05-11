37eb900d5429bffc3767ab322b4ec91c9d899628
James Tin <jamestin100298@gmail.com>
2024-05-11 19:22:09 +0800

    Add background nav image and make text bold in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,25 @@ const Header = () => {
   const [isMenu, setIsMenu] = useState(false);
   console.log(pathname);
   return (
-    <div className="bg-white shadow-lg h-16 flex justify-between items-center md:px-[10%]">
+    // <div className="bg-white shadow-lg h-16 flex justify-between items-center md:px-[10%]">
+    <div
+      className="shadow-lg h-16 flex justify-between items-center md:px-[10%]"
+      style={{
+        backgroundImage: "url('/lead-border.png')",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
       <Link
         href="/"
         className="text-third-color font-bold text-2xl pl-5 md:pl-0"
       >
-        Car Exotics
+        {/* Car Exotics */}
+        <span className="text-third-color">Car</span>
+        {""}
+        <span style={{ color: "orange" }}>LEX</span>
+        {""}
+        <span style={{ color: "red" }}>otics</span>
       </Link>
       <div
         className="md:hidden flex flex-col gap-1 pr-5"
@@ -26,13 +39,14 @@ const Header = () => {
       <div
         className={` ${
           isMenu ? "flex flex-col absolute top-16 w-full" : "hidden md:flex"
-        } gap-5 md:flex-row md:static bg-white md:w-auto text-center`}
+          // } gap-5 md:flex-row md:static bg-white md:w-auto text-center`}
+        } gap-5 md:flex-row md:static md:w-auto text-center`}
       >
         {links?.map((link) => (
           <div key={link.link}>
             <Link
               className={`${
-                pathname === link.link ? "text-third-color" : "text-gray-400"
+                pathname === link.link ? "text-third-color font-bold" : "text-gray-300 font-bold"
               }`}
               href={link.link}
               exact={link.exact}
