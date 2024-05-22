b8122fc816ff1a216c8ce15919c62b13a1f81c5a
James Tin <jamestin100298@gmail.com>
2024-05-22 11:17:30 +0800

    Feature: add sign in sign up page

---
+++ src/app/(auth)/sign-in/page.jsx
@@ -0,0 +1,25 @@
+// import React from "react";
+// // import { Login } from "@/features/Auth/Login";
+
+// const page = () => {
+//   return (
+//     <div>
+//       asd
+//     </div>
+//   );
+// };
+
+// export default page;
+
+import React from "react";
+// import GoogleButton from "@/features/Auth/GoogleButton";
+import { Login } from "@/features/Auth/Login";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+export default LoginPage;
