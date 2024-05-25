7fc1c86bde507069d15b6511ea57c1b66d4af675
James Tin <jamestin100298@gmail.com>
2024-05-25 17:53:11 +0800

    feature libraries: distance calculation in using philippine peso

--- src/lib/distance.js
+++ src/lib/distance.js
@@ -1,4 +1,27 @@
-export const getDistance = (lat1, long1, lat2, long2) => {
+// export const getDistance = (lat1, long1, lat2, long2) => {
+//   const radius = 6371;
+//   const dLat = deg2rad(lat2 - lat1);
+//   const dLong = deg2rad(long2 - long1);
+
+//   const result =
+//     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+//     Math.cos(deg2rad(lat1)) *
+//       Math.cos(deg2rad(lat2)) *
+//       Math.sin(dLong / 2) *
+//       Math.sin(dLong / 2);
+
+//   const c = 2 * Math.atan2(Math.sqrt(result), Math.sqrt(1 - result));
+//   const distance = radius * c;
+//   return distance;
+// };
+
+// function deg2rad(deg) {
+//   return deg * (Math.PI / 180);
+// }
+
+const kilometerToPHPConversionRate = 100;
+
+export const getDistanceInPHP = (lat1, long1, lat2, long2) => {
   const radius = 6371;
   const dLat = deg2rad(lat2 - lat1);
   const dLong = deg2rad(long2 - long1);
@@ -12,7 +35,11 @@ export const getDistance = (lat1, long1, lat2, long2) => {
 
   const c = 2 * Math.atan2(Math.sqrt(result), Math.sqrt(1 - result));
   const distance = radius * c;
-  return distance;
+
+  // Convert distance in kilometers to Philippine Peso
+  const distanceInPHP = distance * kilometerToPHPConversionRate;
+
+  return distanceInPHP;
 };
 
 function deg2rad(deg) {
