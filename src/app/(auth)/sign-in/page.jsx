3634aff9fd72788e354f6a543606009619dd7294
James Tin <jamestin100298@gmail.com>
2024-05-26 12:46:57 +0800

    Fix: authentication have proper routes for sign-in, signup

--- src/app/(auth)/sign-in/page.jsx
+++ src/app/(auth)/sign-in/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 // import React from "react";
 // // import { Login } from "@/features/Auth/Login";
 
@@ -11,11 +13,28 @@
 
 // export default page;
 
-import React from "react";
+import React, { useEffect } from "react";
 // import GoogleButton from "@/features/Auth/GoogleButton";
 import { Login } from "@/features/Auth/Login";
+import { useUserSessionContext } from "../../../context/UserSessionContext"; // Adjust the import path accordingly
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+  const { session } = useUserSessionContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session) {
+      // Redirect to the homepage if the user is authenticated
+      router.push("/");
+    }
+  }, [session, router]);
+
+  // Show a loading state while checking authentication status
+  if (session === undefined) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       <Login />
--- src/app/(auth)/sign-up/page.jsx
+++ src/app/(auth)/sign-up/page.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-import { Signup } from '@/features/Auth/Signup';
+"use client";
+
+import React, { useEffect } from "react";
+import { Signup } from "@/features/Auth/Signup";
+import { useUserSessionContext } from "../../../context/UserSessionContext"; // Adjust the import path accordingly
+import { useRouter } from "next/navigation";
 
 const SignupPage = () => {
+  const { session } = useUserSessionContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session) {
+      // Redirect to the homepage if the user is authenticated
+      router.push("/");
+    }
+  }, [session, router]);
+
   return (
     <div>
       <Signup />
